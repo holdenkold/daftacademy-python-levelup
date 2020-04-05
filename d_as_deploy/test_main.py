1ab9f3a4841ed1a2096f8f22f98007dad872934e
Polina Grinko <grinkopolina@gmail.com>
2020-04-05 16:22:16 +0200

    added second part

--- d_as_deploy/test_main.py
+++ d_as_deploy/test_main.py
@@ -7,3 +7,27 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World during the coronavirus pandemic!"}
+
+
+def test_method_get():
+    response = client.get("/method")
+    assert response.status_code == 200
+    assert response.json() == {"method": "GET"}
+
+
+def test_method_post():
+    response = client.post("/method")
+    assert response.status_code == 200
+    assert response.json() == {"method": "POST"}
+
+
+def test_method_put():
+    response = client.put("/method")
+    assert response.status_code == 200
+    assert response.json() == {"method": "PUT"}
+
+
+def test_method_delete():
+    response = client.delete("/method")
+    assert response.status_code == 200
+    assert response.json() == {"method": "DELETE"}
