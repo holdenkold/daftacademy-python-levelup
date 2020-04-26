07bb6d0098c9ad1f39d50b582c496ed0ab8316d1
Polina Grinko <grinkopolina@gmail.com>
2020-04-27 01:15:02 +0200

    added 5 task

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: sh -c 'cd f_as_feature && uvicorn main:app --host=0.0.0.0 --port=${PORT:-5000}'
+web: sh -c 'cd f_as_feature && uvicorn main:app --workers 1 --host=0.0.0.0 --port=${PORT:-5000}'
--- f_as_feature/main.py
+++ f_as_feature/main.py
@@ -1,15 +1,26 @@
 from fastapi import Cookie, FastAPI, HTTPException,Depends, Request
 from fastapi.responses import Response
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
-import json
+from pydantic import BaseModel
 from functools import wraps
 from hashlib import sha256
 from jinja2 import Template, Environment, FileSystemLoader
+import json
 
 app = FastAPI()
 security = HTTPBasic()
 app.secret_key = "dXNlcl9uYW1lOnBhc3N3b3Jk"
 app.session_token = lambda username, password: sha256(bytes(f"{username}{password}{app.secret_key}", encoding='utf8')).hexdigest()
+app.counter = -1
+app.patients = dict()
+
+class Patient(BaseModel):
+	name: str
+	surename: str
+
+class PatientResponse(BaseModel):
+	id : int = 0
+	patient : Patient
 
 class Credentials:
 	def __init__(self):
@@ -49,8 +60,6 @@ def welcome(request : Request):
 	template = env.get_template('welcome.html')
 	return template.render( user=cr.get_user())
 
-
-@app.get('/login')
 @app.post('/login')
 def login(response: Response, credentials: HTTPBasicCredentials = Depends(security)):
 	if cr.verify(credentials.username, credentials.password):
@@ -62,11 +71,38 @@ def login(response: Response, credentials: HTTPBasicCredentials = Depends(securi
 	else:
 		raise HTTPException(401,'Invalid credentials')
 
-
-@app.get('/logout')
 @app.post('/logout')
 def logout(request : Request, response: Response):
 	response.delete_cookie('session_token')
 	response.status_code = 302
 	response.headers['Location'] = '/'
 	return response
+
+@app.post('/patient')
+@login_required
+def patient_post(patient : Patient, response: Response):
+	app.counter+=1
+	app.patients[app.counter] = patient
+	response.headers['Location'] = f'/patient/{app.counter}'
+	return {"id" : app.counter, "patient" : patient}
+
+@app.get('/patient/')
+@login_required
+def patients_get(pk: int):
+	response = dict()
+	for id, p in app.patients.items():
+		response[f'id_{id}'] = p
+	return response
+
+@app.get('/patient/{pk}', response_model=Patient)
+@login_required
+def patient_get(pk: int):
+	if pk in app.patients:
+		return app.patients[pk]
+	return Response(status_code = 204)	
+
+@app.delete('/patient/{pk}')
+@login_required
+def patient_delete(pk: int):
+	app.counter-=1
+	del app.patients[app.counter] 
