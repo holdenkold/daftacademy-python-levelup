982cf9611525ae4a065a5eba30c76c9e9560d1c9
Polina Grinko <grinkopolina@gmail.com>
2020-04-26 19:33:22 +0200

    fixed bug

--- f_as_feature/main.py
+++ f_as_feature/main.py
@@ -1,9 +1,8 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
 from pydantic import BaseModel
-from fastapi.responses import Response, Request
+from fastapi.responses import Response
 
 app = FastAPI()
-app.patients = dict()
 
 @app.get('/')
 def root():
@@ -11,20 +10,4 @@ def root():
 
 @app.get('/welcome')
 def welcome():
-	return {"message" : "Hello sweety ^.^"}
-
-# @app.api_route(path="/method", methods=["GET", "POST", "PUT", "DELETE"])
-# def show_method(request: Request):
-# 	return {"method": request.method}
-
-# @app.post('/patient')
-# def patient_post(patient : Patient):
-# 	app.counter+=1
-# 	app.patients[app.counter] = patient
-# 	return {"id" : app.counter, "patient" : patient}
-
-# @app.get('/patient/{pk}', response_model=Patient)
-# def patient_get(pk: int):
-# 	if pk in app.patients:
-# 		return app.patients[pk]
-# 	return Response(status_code = 204)	
+	return {"message" : "Welcome sweety ^.^"}	
